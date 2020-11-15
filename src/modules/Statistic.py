6433e725b0957955df8b05155dc13532bc1eddfa
muchang <dale_zhang@foxmail.com>
2020-11-15 11:04:16 +0800

    refine UI

--- src/modules/Statistic.py
+++ src/modules/Statistic.py
@@ -13,7 +13,7 @@ class Statistic:
         self.ignored = 0
 
     def printbar(self):
-        bar = "[time=%ds, mutant=%d, seed=%d, crash=%d, unsound=%d, timeout=%d, ignored=%d]" \
+        bar = "[time:%ds, #mutant:%d, #seed:%d, #crash:%d, #unsound:%d, #timeout:%d, #ignored:%d]" \
               % (time.time()-self.starttime, self.mutants, self.seeds, self.error, self.soundness, self.timeout, self.ignored)
         print(bar, end="\r", flush=True)
     
@@ -21,13 +21,13 @@ class Statistic:
         summary = """
         
 Summary:
-Passed time = %ds
-Generated mutants = %d
-Used seeds = %d
-Crash issues = %d
-Unsound issues = %d
-Timeout cases = %d
-Ignored issues = %d
+Passed time: %ds
+Generated mutants: %d
+Used seeds: %d
+Crash issues: %d
+Unsound issues: %d
+Timeout cases: %d
+Ignored issues: %d
 """ \
         % (time.time()-self.starttime, self.mutants, self.seeds,  self.error, self.soundness, self.timeout, self.ignored)
         print(summary, end="\n", flush=True)
