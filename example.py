3715f1b28508d9be5530cfe3ee4f873c07e610d1
wintered <domwinterer@gmail.com>
2021-03-05 09:55:49 +0100

    Implementing parent pointer for expressions (usage illustrated in example.py)

--- example.py
+++ example.py
@@ -1,12 +1,9 @@
-"""
-Illustrates parser and typechecker on simple SMT formula/script.  
-"""
-import time
-import sys  
-from src.parsing.ast import *  
 from src.parsing.parse import *  
-from src.parsing.typechecker import *  
-
+#
+# Dominik: Illustrates usage of parent pointer.    
+#         @Jiwon: this and the whole algorithm (described on Slack) will need to 
+#          be unit tested with interesting formulas, lets, quantifiers.  
+#
 formula1=\
 """
 (declare-fun x () Int)
@@ -14,3 +11,8 @@ formula1=\
 (assert (= x y))
 """
 formula1, _ = parse_str(formula1,silent=False)
+equals = formula1.commands[2].term
+assert(equals.parent ==  None)
+x = formula1.commands[2].term.subterms[0]
+y = formula1.commands[2].term.subterms[1]
+assert(x.parent == y.parent == equals) 
