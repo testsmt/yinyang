940da2237b00de7a44a78fa9d277af1c1fcc90d5
wintered <domwinterer@gmail.com>
2021-05-06 16:18:06 +0200

    addressing #18

--- tests/unittests/test_parsing.py
+++ tests/unittests/test_parsing.py
@@ -8,8 +8,21 @@ from src.parsing.parse import *
 class ParsingTestCase(unittest.TestCase):
     def test_issue9(self): 
         formula = parse_file("tests/unittests/issue7.smt2")
-        return '\x0a' in formula.__str__() and '\n' in formula.__str__()
+        self.assertTrue('\x0a' in formula.__str__() and '\n' in formula.__str__())
   
+    def test_issue18(self): 
+        formula = parse_file("tests/unittests/issue18.smt2",silent=False)
+        oracle="""\
+(declare-fun a () String)
+(declare-fun b () String)
+(assert (= a b))
+(check-sat)
+(assert (= a b))
+(check-sat)"""
+        self.assertEqual(oracle, formula.__str__())
+        
+
+            
 
 if __name__ == '__main__':
     unittest.main()
