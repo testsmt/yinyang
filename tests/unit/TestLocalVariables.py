5c6a88910e2731ed4f4d7cae7eed541af2047a61
wintered <domwinterer@gmail.com>
2021-07-20 10:45:13 +0200

    adding MIT license header to newly added files

--- tests/unit/TestLocalVariables.py
+++ tests/unit/TestLocalVariables.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) [2020 - 2021] The yinyang authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import unittest
 import sys
 sys.path.append("../../")
@@ -7,7 +29,7 @@ from src.parsing.parse import *
 from src.parsing.typechecker import Context, typecheck
 from src.generators.TypeAwareOpMutation import TypeAwareOpMutation
 from src.generators.GenTypeAwareMutation.GenTypeAwareMutation import *
-from src.generators.GenTypeAwareMutation.util import * 
+from src.generators.GenTypeAwareMutation.util import *
 
 class Mockargs:
     modulo = 3
@@ -35,7 +57,7 @@ class LocalVariableMutationTestCase(unittest.TestCase):
         not_loc = formula.assert_cmd[0].term.subterms[0].subterms[1]
         loc2 = formula.assert_cmd[1].term.subterms[0].subterms[1].subterms[0]
         if not local_compatible(loc1_sub, loc1):
-            return False 
+            return False
         if not local_compatible(not_loc, loc1):
             return False
         if local_compatible(loc1, loc2):
@@ -92,7 +114,7 @@ class LocalVariableMutationTestCase(unittest.TestCase):
         sub2 = declare.subterms[0].subterms[1]
         if not local_compatible(sub1, sub2):  return False
         if local_compatible(declare, sub1): return False
-        return True 
+        return True
 
     def test_parent_pointer(self):
         formula =\
