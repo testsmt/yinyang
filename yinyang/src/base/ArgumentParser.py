cb40d658be2145afddd59297ae992247d469ca5c
muchang <dale_zhang@foxmail.com>
2021-08-18 16:55:02 +0800

    fix rootpath when installing with pypi

--- yinyang/src/base/ArgumentParser.py
+++ yinyang/src/base/ArgumentParser.py
@@ -20,10 +20,17 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import sys
 import argparse
 
 from yinyang.src.base.Exitcodes import ERR_USAGE
 from yinyang.src.base.Version import VERSION, COMMIT
+
+from pathlib import Path
+
+path = Path(__file__)
+ROOTPATH = str(path.parent.absolute().parent.parent.parent)
+sys.path.append(ROOTPATH)
 
 
 class ArgumentParser(argparse.ArgumentParser):
@@ -183,40 +190,40 @@ def add_yinyang_args(parser, rootpath, current_dir):
     )
 
 
-def build_opfuzz_parser(rootpath, current_dir, usage):
+def build_opfuzz_parser(current_dir, usage):
     parser = ArgumentParser(
         description="",
         usage=usage,
         formatter_class=argparse.RawDescriptionHelpFormatter,
         add_help=False,
     )
-    add_common_args(parser, rootpath, current_dir)
-    add_opfuzz_args(parser, rootpath, current_dir)
+    add_common_args(parser, ROOTPATH, current_dir)
+    add_opfuzz_args(parser, ROOTPATH, current_dir)
 
     return parser
 
 
-def build_typefuzz_parser(rootpath, current_dir, usage):
+def build_typefuzz_parser(current_dir, usage):
     parser = ArgumentParser(
         description="",
         usage=usage,
         formatter_class=argparse.RawDescriptionHelpFormatter,
         add_help=False,
     )
-    add_common_args(parser, current_dir, rootpath)
-    add_typefuzz_args(parser, current_dir, rootpath)
+    add_common_args(parser, ROOTPATH, current_dir)
+    add_typefuzz_args(parser, ROOTPATH, current_dir)
 
     return parser
 
 
-def build_yinyang_parser(rootpath, current_dir, usage):
+def build_yinyang_parser(current_dir, usage):
     parser = ArgumentParser(
         description="",
         usage=usage,
         formatter_class=argparse.RawDescriptionHelpFormatter,
         add_help=False,
     )
-    add_common_args(parser, current_dir, rootpath)
-    add_yinyang_args(parser, current_dir, rootpath)
+    add_common_args(parser, ROOTPATH, current_dir)
+    add_yinyang_args(parser, ROOTPATH, current_dir)
 
     return parser
